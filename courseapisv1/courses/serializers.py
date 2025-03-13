933660198994202b2282174a1125967debaf7a10
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2025-03-13 09:21:24 +0700

    BTH 2 done

--- courseapisv1/courses/serializers.py
+++ courseapisv1/courses/serializers.py
@@ -1,4 +1,4 @@
-from courses.models import Category, Course, Lesson, Tag, User
+from courses.models import Category, Course, Lesson, Tag, User, Comment
 from rest_framework import serializers
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -34,7 +34,33 @@ class LessonDetailSerializer(LessonSerializer):
         model = LessonSerializer.Meta.model
         fields = LessonSerializer.Meta.fields + ['content', 'tags']
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['first_name', 'last_name', 'username', 'password', 'email']
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'password', 'avatar']
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+    def create(self, validated_data):
+        data = validated_data.copy()
+        u = User(**data)
+        u.set_password(u.password)
+        u.save()
+
+        return u
+
+    def to_representation(self, instance):
+        d = super().to_representation(instance)
+        d['avatar'] = instance.avatar.url
+        return d
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'content', 'created_date', 'updated_date', 'user']
--- courseapisv1/courses/views.py
+++ courseapisv1/courses/views.py
@@ -1,9 +1,11 @@
 from email.policy import default
+
+from django.template.defaulttags import comment
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from courses.models import Category, Course, Lesson
+from courses.models import Category, Course, Lesson, User
 from courses import serializers, paginators
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, parsers
 
 class CategoryViewSet(viewsets.ViewSet, generics.ListAPIView):
     queryset = Category.objects.filter(active=True)
@@ -33,4 +35,13 @@ class LessonViewSet(viewsets.ViewSet, generics.RetrieveAPIView):
     queryset = Lesson.objects.prefetch_related('tags').filter(active=True)
     serializer_class = serializers.LessonDetailSerializer
 
-# class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
+    @action(methods=['get'], url_path='comments', detail=True)
+    def get_comments(self, request, pk):
+        comments = self.get_object().comment_set.select_related('user').filter(active=True)
+        return Response(serializers.CommentSerializer(comments, many=True).data)
+
+
+class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = serializers.UserSerializer
+    parser_classes = [parsers.MultiPartParser]
